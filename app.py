0d8e8939158eccc52e1cd4f7dd53adb015b2ed3d
Mark Fontana <markfontana@gmail.com>
2016-03-11 12:28:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure, output_notebook, show
+from bokeh.embed import components
 import numpy as np
 import pandas as pd
 import Quandl
